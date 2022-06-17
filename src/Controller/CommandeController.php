696cb08a87f9da6deb127fb3ddc838d0a517ea8d
demba <papadembandoye087@gmail.com>
2022-06-17 22:37:24 +0000

    tester

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -14,9 +14,9 @@ class CommandeController extends AbstractController
     #[Route('/commande', name: 'app_commande')]
     public function index(CommandeRepository $em, PaginatorInterface $paginator, Request $request): Response
     {
-
+        $user = array_values((array)$this->getUser())[0];
         $commandes=$paginator->paginate(
-            $em->findAll(),
+            $em->findBy(['etat'=>'En cours' , 'user' => $user]),
             $request->query->getInt('page', 1),
             5
         );
@@ -24,4 +24,41 @@ class CommandeController extends AbstractController
             'commandes' => $commandes,
         ]);
     }
+    #[Route('/commande/Annuler', name: 'app_commande_Annuler')]
+    public function commandeAnnuler(CommandeRepository $em, PaginatorInterface $paginator, Request $request): Response
+    {
+        $user = array_values((array)$this->getUser())[0];
+        $commandes=$paginator->paginate(
+            $em->findBy(['etat'=>'Annuler' , 'user' => $user]),
+            $request->query->getInt('page', 1),
+            5
+        );
+        return $this->render('commande/listerCommande.html.twig', [
+            'commandes' => $commandes,
+        ]);
+    }
+    #[Route('/commande/Terminer', name: 'app_commande_terminer')]
+    public function commandeTerminer(CommandeRepository $em, PaginatorInterface $paginator, Request $request): Response
+    {
+        $user = array_values((array)$this->getUser())[0];
+        $commandes=$paginator->paginate(
+            $em->findBy(['etat'=>'Terminer' , 'user' => $user]),
+            $request->query->getInt('page', 1),
+            5
+        );
+        return $this->render('commande/listerCommande.html.twig', [
+            'commandes' => $commandes,
+        ]);
+    }
+    // #[Route('/commande', name: 'app_commande')]
+    // public function commandeUser(CommandeRepository $commande): Response
+    // {
+    //     $user = array_values((array)$this->getUser())[0];
+    //     $commandes = $commande->findBy(['etat'=>'encours' , 'user' => $user]);
+    //     return $this->render('commande/listerCommande.html.twig', [
+    //         'controller_name' => 'CommandeController',
+    //         'commandes'=>$commandes,
+          
+    //     ]);
+    // }
 }
